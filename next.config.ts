f0934da3e2b51b3d48bb6b6f6152951da3ca898a
code <kabir.naved.khan@gmail.com>
2025-01-06 21:58:22 +0530

    completed bidPark

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,6 @@ import type { NextConfig } from "next";
 
 
 const nextConfig: NextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images:{
     dangerouslyAllowSVG: true,
     remotePatterns: [
